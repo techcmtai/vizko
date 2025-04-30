a6c5dd666a321b287d182d47c46d7b62836eb177
Rahul Kumar <rk93272722@gmail.com>
2025-04-30 13:16:25 +0530

    1

--- server/index.ts
+++ server/index.ts
@@ -1,10 +1,16 @@
 import express, { type Request, Response, NextFunction } from "express";
 import { registerRoutes } from "./routes";
 import { setupVite, serveStatic, log } from "./vite";
+import cors from "cors";
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+app.use(cors({
+  origin: process.env.FRONTEND_URL,
+  credentials: true
+}));
 
 app.use((req, res, next) => {
   const start = Date.now();
